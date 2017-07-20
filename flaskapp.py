6a9f9b14f98472c7e6250f4e067a7e2fff59aa13
pablo1n7 <pablo1n7@gmail.com>
2017-07-20 19:23:31 -0300

    cambios sugeridos, uso de socketIO en todas las funciones

--- flaskapp.py
+++ flaskapp.py
@@ -3,7 +3,7 @@ import json
 from models.models import Usuario, Lugar, Mensaje
 from flask import Flask, request,\
      render_template
-from flask_socketio import SocketIO,emit
+from flask_socketio import SocketIO, emit, disconnect
 
 app = Flask(__name__, static_folder='statics')
 app.config.from_pyfile('flaskapp.cfg',)
@@ -15,40 +15,6 @@ def index():
     ''' Retorna la pagina principal '''
     return render_template("index.html")
 
-
-@app.route("/getlugares/<usuario_nombre>")
-@app.route("/getlugares")
-def get_lugares(usuario_nombre=None):
-    '''
-        Retorna los lugares agregados por un usuario
-            si usuario_nombre!=None regresa todos  los lugares
-    '''
-    lugares = Lugar.get_lugares(usuario_nombre)
-    return json.dumps(lugares)
-
-@app.route("/getusuarios")
-def get_usuarios():
-    '''
-        Retorna los usuarios    
-    '''
-    usuarios = Usuario.get_usuarios()
-    return json.dumps(usuarios)
-
-
-@app.route("/guardarlugar",methods=['POST'])
-def guardar_lugar():
-    '''
-
-        TODO: si vamos a hacer un login en el dispositivo, el metodo 'guardarlugar'
-            recibiría un id en vez de un nombre o algo asi. Me parece excesivo igual
-
-        Metodo que guarda un lugar. El json que se envia por POST tiene la siguiente forma:
-            {"nombre": "CASA", "latlng": "2222/222", "descripcion": "es mi lugar", "usuario":"pablo1n7"}
-
-    '''
-    lugar = Lugar(request.get_json())
-    lugar.guardar()
-    return json.dumps({'status':'OK'})
 
 @app.route("/guardarusuario",methods=['POST'])
 def guardar_usuario():
@@ -63,43 +29,83 @@ def guardar_usuario():
         usr = Usuario(request.get_json())
         usr.guardar()
     except Exception:
-        return json.dumps({'estado': 'ERROR', 'descripcion':'Usuario en uso'})
-    return json.dumps({'estado':'OK'})
+        return json.dumps({'codigo':500, 'descripcion':'ERROR:Usuario en uso'})
+    return json.dumps({'codigo':'200'})
 
-
-@socketio.on('message')
-def enviar_mensaje(origen, destinatorio, texto_mensaje):
+@socketio.on('getlugares')
+def get_lugares(usuario_nombre=None):
     '''
-        Mensajeria por socketIO. Evento "message".
+        Retorna los lugares agregados por un usuario
+            si usuario_nombre!=None regresa todos  los lugares
+    '''
+    lugares = Lugar.get_lugares(usuario_nombre)
+    emit("getlugares", json.dumps(lugares), room=request.sid)
+
+@socketio.on('getusuarios')
+def get_usuarios():
+    '''
+        Retorna los usuarios    
+    '''
+    usuarios = Usuario.get_usuarios()
+    emit("getusuarios", json.dumps(usuarios), room=request.sid)
+
+#@app.route("/guardarlugar",methods=['POST'])
+@socketio.on('guardarlugar')
+def guardar_lugar(nombre, latlng, descripcion, usuario):
+    '''
+        Metodo que guarda un lugar. El json que se envia por POST tiene la siguiente forma:
+            {"nombre": "CASA", "latlng": "2222/222", "descripcion": "es mi lugar",
+            "usuario":"pablo1n7"}
+
+    '''
+    data = {"nombre": nombre, "latlng": latlng, "descripcion": descripcion, "usuario":usuario}
+    lugar = Lugar(data)
+    id_lugar = lugar.guardar()
+    emit("guardarlugar", {'codigo':'200', 'id_lugar':str(id_lugar)}, room=request.sid)
+
+@socketio.on('mensaje')
+def enviar_mensaje(destinatorio, texto_mensaje,id_lugar=None):
+    '''
+        Mensajeria por socketIO. Evento "mensaje".
     '''
     usuario_destino = Usuario.get_usuario(destinatorio)
-    usuario_origen = Usuario.get_usuario(origen)
-    mensaje = Mensaje(usuario_origen.id_usuario, usuario_destino.id_usuario, texto_mensaje)
+    mensaje = Mensaje(usuario_destino.id_usuario, texto_mensaje, id_lugar)
     try:
         socket_destino = clients[usuario_destino.id_usuario]
-        emit("message", {"origen":origen, "mensaje":texto_mensaje}, room=socket_destino)
+        for key in list(clients.keys()):
+            if clients[key] == request.sid:
+                origen = key
+        emit("mensaje", {"origen":origen, "mensaje":texto_mensaje, "id_lugar":id_lugar}, 
+             room=socket_destino)
         mensaje.estado = "enviado"
         mensaje.guardar()
     except KeyError:
         mensaje.guardar()
-        
 
 @socketio.on('connect')
-def connect():
+def conectar():
     '''
         Metodo de conexion, que ademas registra las conexiones activas.
     '''
     user_id = request.args.get('user_id', '')
-    usuario = Usuario.get_usuario_por_id(user_id)
+    try:
+        usuario = Usuario.get_usuario_por_id(user_id)
+        emit("connect", {'status':'OK','usuario':usuario.nombre}, room=request.sid)
+    except Exception:
+        emit("no_registrado", {'codigo':404, 'descripcion':'ERROR: usuario no encontrado'}, room=request.sid)
+        #disconnect()
+        return
+
     clients[user_id] = request.sid
     mensajes_sin_enviar = Mensaje.get_mensajes(usuario.id_usuario)
-    for m in mensajes_sin_enviar:
-        emit("message", {"origen":Usuario.get_usuario_por_id(m.id_origen).nombre, "mensaje":m.mensaje}, room=request.sid)
-        m.registrar_envio()
+    for msg in mensajes_sin_enviar:
+        emit("mensaje", {"origen":Usuario.get_usuario_por_id(msg.id_origen).nombre,
+                         "mensaje":msg.mensaje,"id_lugar":msg.id_lugar}, room=request.sid)
+        msg.registrar_envio()
     print('Client Connect {}'.format(usuario.nombre))
 
 @socketio.on('disconnect')
-def disconnect():
+def desconectar():
     '''
         Metodo de desconeccion, elimina la conexion de la lista de conexiones activas.
     '''
@@ -110,6 +116,7 @@ def disconnect():
 
 def main():
     '''Metodo principal'''
+    #socketio.run(app,host="192.168.0.9",port=80)
     socketio.run(app)
 
 if __name__ == "__main__":
--- models/models.py
+++ models/models.py
@@ -2,13 +2,13 @@
 import os
 from pymongo import MongoClient
 
-''' Configuración de la base '''
+#Configuración de la base
 if 'OPENSHIFT_APP_NAME' in os.environ:
     CLIENT = MongoClient(os.environ['OPENSHIFT_MONGODB_DB_URL'])
 else:
     CLIENT = MongoClient('mongodb://localhost:27017/')
 DB = CLIENT.pickmeapp
-''' ----------------------- '''
+#-----------------------
 
 class Lugar(object):
     '''
@@ -27,7 +27,7 @@ class Lugar(object):
             obtiene los lugares agregados por ese usuario.
         '''
         if  filtro_usuario != None:
-            filtro  = {"usuario":filtro_usuario}
+            filtro = {"usuario":filtro_usuario}
         else:
             filtro = {}
         return [Lugar(r).diccionarizar() for r in list(DB.lugares.find(filtro))]
@@ -37,7 +37,6 @@ class Lugar(object):
         '''
             Obtiene el lugar que tiene el nombre ingresado
         '''
-        
         return [Lugar(r).diccionarizar() for r in list(DB.lugares.find({"nombre":filtro_nombre}))]
 
     def __init__(self, data):
@@ -48,6 +47,10 @@ class Lugar(object):
         self.descripcion = data["descripcion"]
         self.latlng = data["latlng"]
         self.usuario = data["usuario"]
+        try:
+            self.id_lugar = str(data['_id'])
+        except KeyError:
+            return
 
     def guardar(self):
         '''
@@ -60,7 +63,7 @@ class Lugar(object):
             Prepara los objetos para enviar.
         '''
         d_lugar = self.__dict__
-        d_lugar['_id'] = ""
+        #d_lugar['_id'] = str(d_lugar['_id'])
         return d_lugar
 
 
@@ -85,7 +88,7 @@ class Usuario(object):
             Obtiene un usuario de la base segun su id de usuario.
         '''
         usuario = [Usuario(r) for r in list(DB.usuarios.find({"id_usuario":id_usuario}))]
-        return usuario[0] 
+        return usuario[0]
 
     @classmethod
     def get_usuarios(cls):
@@ -97,7 +100,7 @@ class Usuario(object):
 
 
     @classmethod
-    def check_usuario(cls, nombre_usuario,id_usuario):
+    def check_usuario(cls, nombre_usuario, id_usuario):
         '''
            Check_usuario comprueba que el nombre de usuario sea unico.
            Devuelve True si esta disponible para usar.
@@ -120,7 +123,7 @@ class Usuario(object):
         '''
             Guarda un lugar en la base de datos
         '''
-        if not Usuario.check_usuario(self.nombre,self.id_usuario):
+        if not Usuario.check_usuario(self.nombre, self.id_usuario):
             raise Exception
         return DB.usuarios.insert_one(self.__dict__).inserted_id
 
@@ -129,7 +132,6 @@ class Usuario(object):
             Prepara los objetos para enviar.
         '''
         d_usuario = self.__dict__
-        d_usuario['_id'] = ""
         return d_usuario
 
 class Mensaje(object):
@@ -142,15 +144,16 @@ class Mensaje(object):
     estado = ""
 
     @classmethod
-    def get_mensajes(cls,id_destino):
+    def get_mensajes(cls, id_destino):
         '''
             Obtiene todos los mensajes no enviados de la base con ese destino.
         '''
-        mensajes = [Mensaje(r['id_origen'], r['id_destino'], r['mensaje'], r['estado']) 
-                    for r in list(DB.mensajes.find({"id_destino":id_destino, "estado":"no_enviado"}))]
+        mensajes = [Mensaje(r['id_origen'], r['id_destino'], r['mensaje'], r['estado'])
+                    for r in list(DB.mensajes.find({"id_destino":id_destino,
+                                                    "estado":"no_enviado"}))]
         return mensajes
 
-    def __init__(self, _id_origen, _id_destino, mensaje, estado="no_enviado"):
+    def __init__(self, _id_origen, _id_destino, mensaje, estado="no_enviado", id_lugar=None):
         '''
             Constructor de la clase Mensaje
         '''
@@ -158,6 +161,7 @@ class Mensaje(object):
         self.id_origen = _id_origen
         self.mensaje = mensaje
         self.estado = estado
+        self.id_lugar = id_lugar
 
     def guardar(self):
         '''
@@ -167,7 +171,7 @@ class Mensaje(object):
 
     def registrar_envio(self):
         '''
-            Registra en la base el mensaje como enviado 
+            Registra en la base el mensaje como enviado
         '''
         mensaje_previo = {}
         mensaje_previo["id_origen"] = self.id_origen
